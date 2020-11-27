cc8e819b57d3fcd523b74ce95a5f8b8e311da88c
Colby Trahan <ctrahan94@users.noreply.github.com>
2020-11-26 18:01:01 -0700

    Updated the navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,29 +6,43 @@ import "./style.css";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <Link className="navbar-brand" to="/">Colby Trahan</Link>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ml-auto">
-      <li className="nav-item">
+      <Link className="navbar-brand" to="/">
+        Colby Trahan
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
             <Link
               to="/"
               className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
-                  ? "nav-link active"
+                window.location.pathname === "/" ||
+                window.location.pathname === "/home"
+                  ? "nav-link"
                   : "nav-link"
               }
             >
               Home
-              </Link>
-              </li>
-              <li className="nav-item">
+            </Link>
+          </li>
+          <li className="nav-item">
             <Link
               to="/portfolio"
-              className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+              className={
+                window.location.pathname === "/portfolio"
+                  ? "nav-link"
+                  : "nav-link"
+              }
             >
               Portfolio
             </Link>
@@ -36,14 +50,18 @@ function Navbar() {
           <li className="nav-item">
             <Link
               to="/contact"
-              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+              className={
+                window.location.pathname === "/contact"
+                  ? "nav-link"
+                  : "nav-link"
+              }
             >
               Contact
             </Link>
           </li>
-      </ul>
-    </div>
-  </nav>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
