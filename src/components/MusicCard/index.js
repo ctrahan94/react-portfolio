47863c8f16aefd64f9f98191178d2b2c6642be8e
Colby Trahan <ctrahan94@users.noreply.github.com>
2020-12-11 13:12:55 -0700

    Updating playlist generator card

--- src/components/MusicCard/index.js
+++ src/components/MusicCard/index.js
@@ -10,7 +10,10 @@ function MusicCard() {
               <h5 className="card-title">Playlist Generator</h5>
               <p>
                 <span>Description:</span> An app that allows you to search for songs, albums, artists and generate a playlist that is copied to your clipboard.
-
+              </p>
+              <p>
+              <span>Role:</span>Used jQuery AJAX method to make requests to server-side APIs. Created an HTML page and styled with CSS and integrated Deezer API and manipulated the DOM using JavaScript leveraging the DOM API. 
+              <span>Tools Utilized:</span> HTML, CSS, JSON,  AJAX, HTTP GET requests, and Server-side APIs
               </p>
               <p className="card-text">
                 <a
